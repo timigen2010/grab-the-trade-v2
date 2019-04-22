008beb026b9856e6ea7d58626686cac70090b674
Mikhail <timigen2010@yandex.ua>
2019-04-22 23:16:48 +0300

    DBModule + First Migration
    Signed-off-by: Mikhail <timigen2010@yandex.ua>

---
+++ db/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Key(models.Model):
+    key = models.CharField(max_length=255, unique=True)
+    date_used = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+
+class Resource(models.Model):
+    url = models.URLField(max_length=255)
+
+
+class Page(models.Model):
+    url = models.URLField(max_length=255)
+    date_used = models.DateTimeField(auto_now_add=True)
+
+    resource = models.ForeignKey(Resource, null=True, on_delete=models.SET_NULL)
+
+
+class ArticleCategory(models.Model):
+    key = models.ForeignKey(Key, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    article = models.ForeignKey(Article, null=True, on_delete=models.SET_NULL)
+    page = models.ForeignKey(Page, null=True, on_delete=models.SET_NULL)
+
--- gtt/settings.py
+++ gtt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'gtt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gtt',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
@@ -119,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from gtt.local_settings import *
+except ImportError:
+    pass
